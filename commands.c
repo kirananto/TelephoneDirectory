2a24e97bbe6aa5867861b0c18c8fc12f52ebb8bd
Jovin Jijo <jovinjijo@users.noreply.github.com>
2017-05-12 01:39:59 +0530

    Update commands.c

--- commands.c
+++ commands.c
@@ -7,42 +7,45 @@
 
 AddressBookList * commandLoad(char * fileName)
 {
-    /**
-     * Loads the given file and returns a new AddressBookList.
-     *
-     * If the file doesn't exist or corruption is found in the file
-     * then NULL is returned.
-     */
-     int i, j;
-     FILE *data;
-     printf("> Loading the file ... \n");
-     if((data = fopen(fileName, "r"))) {
-    	char parsedLine[MAX_LINE_LENGTH];
-    	while (fgets(parsedLine, MAX_LINE_LENGTH, data) != NULL) {
-    		if(parsedLine[0]=='#')
-			{
-			    continue;
-			} else
+	/**
+	 * Loads the given file and returns a new AddressBookList.
+	 *
+	 * If the file doesn't exist or corruption is found in the file
+	 * then NULL is returned.
+	 */
+	int i, j;
+	FILE *data;
+	printf("> Loading the file ... \n");
+	if((data = fopen(fileName, "r")))
+	{
+		char parsedLine[MAX_LINE_LENGTH];
+		while (fgets(parsedLine, MAX_LINE_LENGTH, data) != NULL)
+		{
+			if(parsedLine[0]=='#')
+				continue;
+			else
 			{
 				char *getId = strtok(parsedLine, ", ");
-    			char *getName = strtok(NULL, ", ");
+				char *getName = strtok(NULL, ", ");
 				char *getTelephone[20];
-                i = 0;
-    			while((getTelephone[i] = strtok(NULL, ", ")) != NULL)
-                        i++;
-			printf("%s - %s\n",getId,getName);
-            for(j = 0; j <i ; j++)
-            {
-                printf(" - %s\n", getTelephone[j]);
-            }
+				i = 0;
+				while((getTelephone[i] = strtok(NULL, ", ")) != NULL)
+					i++;
+				printf("%s - %s\n",getId,getName);
+				for(j = 0; j <i ; j++)
+				{
+					printf(" - %s\n", getTelephone[j]);
+				}
+			}
 		}
-    }
-	fclose(data);
-    } else {
- 	 /*printf("> Error: The specified file is in the wrong format and cannot be loaded.\n");*/
-     printf("> Error: File not found\n");
+		fclose(data);
 	}
-    return NULL;
+	else
+	{
+		/*printf("> Error: The specified file is in the wrong format and cannot be loaded.\n");*/
+		printf("> Error: File not found\n");
+	}
+	return NULL;
 }
 
 void commandUnload(AddressBookList * list)
@@ -73,34 +76,34 @@ void commandRemove(AddressBookList * list, char * telephone)
 { }
 
 void commandSort(
-    AddressBookList * list,
-    int sort(const void * node, const void * otherNode))
+	AddressBookList * list,
+	int sort(const void * node, const void * otherNode))
 {
-    /* Sort the nodes within list in ascending order using the
-     * provided sort function to compare nodes.
-     */
+	/* Sort the nodes within list in ascending order using the
+	 * provided sort function to compare nodes.
+	 */
 }
 
 int compareName(const void * node, const void * otherNode)
 {
-    /* Compare node name with otherNode name.
-     *
-     * return < 0 when node name is smaller than otherNode name.
-     * return 0 when the names are equal.
-     * return > 0 when node name is bigger than otherNode name.
-     */
-    return 0;
+	/* Compare node name with otherNode name.
+	 *
+	 * return < 0 when node name is smaller than otherNode name.
+	 * return 0 when the names are equal.
+	 * return > 0 when node name is bigger than otherNode name.
+	 */
+	return 0;
 }
 
 int compareID(const void * node, const void * otherNode)
 {
-    /* Compare node id with otherNode id.
-     *
-     * return < 0 when node id is smaller than otherNode id.
-     * return 0 when the ids are equal.
-     * return > 0 when node id is bigger than otherNode id.
-     */
-    return 0;
+	/* Compare node id with otherNode id.
+	 *
+	 * return < 0 when node id is smaller than otherNode id.
+	 * return 0 when the ids are equal.
+	 * return > 0 when node id is bigger than otherNode id.
+	 */
+	return 0;
 }
 
 void commandSave(AddressBookList * list, char * fileName)
